74eb35680ab4968fa1791e44d5cc2ed4ac678ad5
Michael Venema <michael.venema2010@gmail.com>
2020-06-09 16:14:53 -0700

    Update silent_upgrade.sh

--- silent_upgrade.sh
+++ silent_upgrade.sh
@@ -1,4 +1,4 @@
-!#/bin/bash
+#!/bin/bash
 echo "$USER ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -qq git htop < /dev/null > /dev/null
