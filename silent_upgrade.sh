fd52c8eb5130abe885a600126cc48ec653b2a6ef
Michael Venema <michael.venema2010@gmail.com>
2020-06-09 16:17:02 -0700

    Update silent_upgrade.sh

--- silent_upgrade.sh
+++ silent_upgrade.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-echo "$USER ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers
+
+
+
+echo "Do you want to add yourself to the sudoers group? [Y,n]"
+
+read input
+if [[ $input == "Y" || $input == "y" ]]; then
+        echo "$USER ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers
+else
+        echo "Okay moving on you will be prompted for your password!"
+fi
+
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -qq git htop < /dev/null > /dev/null
