ec91bf999a4683c17c6b5b8d840d29c0dd53f890
Michael Venema <michael.venema2010@gmail.com>
2020-06-10 11:08:26 -0700

    Update silent_upgrade.sh

--- silent_upgrade.sh
+++ silent_upgrade.sh
@@ -1,3 +1,4 @@
 !#/bin/bash
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -qq < /dev/null > /dev/null
+sudo DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -qq < /dev/null > /dev/null
